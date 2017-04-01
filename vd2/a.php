212c1e25f03fb11d0c44aa120e35da8dd9a60755
HAI-PC\HAI <mjnhaj95@gmail.com>
2017-04-01 17:46:39 +0700

    demo vd2 .get

---
+++ vd2/a.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>vd1</title>
+    <script src="../jquery-3.1.1.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $.post("b.php", {ho:"LeMinhHai"}, function(data){
+                $("#noidung").html(data);
+            });
+        });
+    </script>
+</head>
+<body>
+    <p>AAAAAAA</p>
+    <p><a href="b.php">Go to B</a></p>
+    <div id="noidung">....</div>
+</body>
+</html>
